cbdc707ed7eea80a456bffe6b0606a1a22fd0c4a
Alan Ruvalcaba <aruval3@gmail.com>
2015-03-29 23:56:13 -0500

    Added provisional group permissions

---
+++ app/database/seeds/GroupSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+class GroupSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+    {
+        $this->command->info('Importing groups...');
+
+        //Admin
+        Sentry::getGroupProvider()->create(array(
+            'name'=>'Admin',
+            'permissions'=>array('admin'=>1)));
+
+        //Teachers
+        Sentry::getGroupProvider()->create(array(
+            'name'=>'Teacher',
+            'permissions'=>array('teacher'=>1)));
+
+        //Students
+        Sentry::getGroupProvider()->create(array(
+            'name'=>'Student',
+            'permissions'=>array('student'=>1));)
+	}
+
+}
