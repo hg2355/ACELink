d7613736bd0986ba34803b436f919d567db2d2ef
Alan Ruvalcaba <aruval3@gmail.com>
2015-04-12 23:25:04 -0500

    Rewording

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -1,4 +1,4 @@
 <body>
 Dear {{ $user->first_name }} {{ $user->last_name }}, <br>
-Welcome to teachtogeher! Use the password : {{ $password }} to login.
+Welcome to teachtogether! Use the password : {{ $password }} to login.
 </body>
--- app/views/pages/teacher-login.blade.php
+++ app/views/pages/teacher-login.blade.php
@@ -155,7 +155,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h2 class="modal-title" id="signupSuccessModalLabel">Congratulations! New teacher is created successfully.<br>Login credentials are sent to your email address.<br>Please login as a teacher.</h2>
+        <h2 class="modal-title" id="signupSuccessModalLabel">Signup successful. Login credentials were sent to your email address.
       </div>
       <div class="modal-footer">
         <span type="button" class="btn btn-success" data-dismiss="modal">OK</span>
