36fa378343a72e83a1c9bbb60006158add81e167
Alan Ruvalcaba <aruval3@gmail.com>
2015-04-24 22:20:52 +0000

    stars

--- app/views/js/rate.blade.php
+++ app/views/js/rate.blade.php
@@ -1,6 +1,6 @@
 <script src="https://s3.amazonaws.com/teachtogether.co/assets/js/jquery.min.js"></script>
 <script src="https://s3.amazonaws.com/teachtogether.co/assets/js/bootstrap.min.js"></script>
-<script src="https://s3.amazonaws.com/teachtogether.co/assets/js/bootstrap-slider.js"></script>
+<script src="https://s3.amazonaws.com/teachtogether.co/assets/js/star-rating.min.js"></script>
 <script>
 $(document).ready(function() {
 $('#rating').slider({value: 0});
