87f88237a4319fa8638d7422ed2d1e053e5bc0e5
Alan Ruvalcaba <aruval3@gmail.com>
2015-04-27 21:56:38 -0500

    Added additional admin stats

--- app/models/Activity.php
+++ app/models/Activity.php
@@ -1,5 +1,7 @@
 <?php namespace TT\Models;
 
+use TT\Models\Student;
+
 class Activity extends \Eloquent
 {
     protected $table = 'activities';
@@ -21,4 +23,70 @@ class Activity extends \Eloquent
             return 0;
         }
     }
+
+    public function q1Percent()
+    {
+        try
+        {
+            $total = \DB::table('activities_surveys')->where('activity_id','=',$this->id)->count();
+
+            if( $total === 0 )
+                return 0;
+
+            $funCount = \DB::table('activities_surveys')->where('activity_id','=',$this->id)
+                                                        ->where('q1','=',1)->count();
+            
+            return bcdiv($funCount,$total,3)*100;
+        }
+
+        catch(\Exception $ex)
+        {
+            \Log::error($ex);
+            return 0;
+        }
+    }
+
+    public function q2Percent()
+    {
+        try
+        {
+            $total = \DB::table('activities_surveys')->where('activity_id','=',$this->id)->count();
+
+            if( $total === 0)
+                return 0;
+
+            $appropriateCount = \DB::table('activities_surveys')->where('activity_id','=',$this->id)
+                                                                ->where('q2','=',1)->count();
+            return bcdiv($appropriateCount,$total,1)*100;
+
+        }
+
+        catch(\Exception $ex)
+        {
+            \Log::error($ex);
+            return 0;
+        }
+    }
+
+    public function finishedPercent()
+    {
+        try
+        {
+            $students = Student::where('traits_type','=','TT\Models\StudentTrait')->select('id');
+
+            $studentsFinished = \DB::table('users_activities')->select('id')->distinct();
+
+            $total = bcdiv(count($studentsFinished),count($students),3)*100;
+
+            return $total;
+        }
+
+        catch(\Exception $ex)
+        {
+            \Log::error($ex);
+            return 0;
+        }
+
+    }     
+    
 }
--- app/views/pages/home/admin.blade.php
+++ app/views/pages/home/admin.blade.php
@@ -19,10 +19,14 @@ else if( Session::has('success') )
 
 
 <a href="/activity/create" class="btn btn-large btn-danger">Upload New Activity</a>
+<div class="table-responsive" style="margin-top: 15px">
 <table class="table table-striped table-bordered" style="margin-top: 15px">
 <tr>
     <td>Title</td>
     <td>Avg. Rating</td>
+    <td>% Fun for Q1</td>
+    <td>% Appropriate for Q2</td>
+    <td>% Parents who finished this activity</td>
     <td>Actions</td>
 <?php 
 
@@ -31,15 +35,20 @@ foreach($activities as $activity)
     echo '<tr id="'.$activity->title.'">';
     echo '<td>'.$activity->title.'</td>';
     $rating = $activity->avgRating();
+    
     echo '<td>'.$rating.'</td>';
-    $deleteString = "'".$activity->id."'".",'activity','".$activity->title."'";
+    echo '<td>'.$activity->q1Percent().'%</td>';
+    echo '<td>'.$activity->q2Percent().'%</td>';
+    echo '<td>'.$activity->finishedPercent().'%</td>';
 
+    $deleteString = "'".$activity->id."'".",'activity','".$activity->title."'";
     echo '<td><a href="'.URL::route('activity.edit',[$activity->id]).'" class="btn btn-warning">Edit</a><a href="#" data-token="'.csrf_token().'" class="btn btn-danger" style="margin-left: 15px" onclick="destroy('.$deleteString.')">Delete</a></td>';
     echo '</tr>';
 }
 ?>
 </tr>
 </table>
+</div>
 </div>
 </section>
 
